5d8ee47ba9987ba93a5b056433395a6046e3ef2a
Nicolas Barbier <nba67000@gmail.com>
2022-12-14 18:13:58 +0100

    create new cicd files

---
+++ cicd/apply_terraform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fail on any error
+set -eu
+
+# go to the main folder to use terraform commands
+cd jupiter-website-ecs 
+
+# initialize terraform
+terraform init
+
+# # apply terraform
+terraform apply -auto-approve
+
+# destroy terraform
+# terraform destroy -auto-approve
